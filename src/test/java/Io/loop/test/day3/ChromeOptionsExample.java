7cf4be0faee6df8b9e143e023d755e27807e8edf
gulnar84 <gulnarresearcher@gmail.com>
2025-01-24 14:25:09 -0500

    My first automation4

---
+++ src/test/java/Io/loop/test/day3/ChromeOptionsExample.java
@@ -0,0 +1,22 @@
+package Io.loop.test.day3;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+
+public class ChromeOptionsExample {
+    public static void main(String[] args) {
+        //create an object of chrome options
+        ChromeOptions options = new ChromeOptions();
+
+        // add argument to that option
+        options.addArguments("--start-maximized");
+        //start chrome with that option
+        WebDriver driver = new ChromeDriver(options);
+        // navigate
+        driver.get("https://www.google.com");
+
+
+    }
+}
---
+++ src/test/java/Io/loop/test/day3/GoogleSearch.java
@@ -0,0 +1,52 @@
+package Io.loop.test.day3;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GoogleSearch {
+    public static void main(String[] args) {
+        /*
+         * Navigate to Google search
+         * maximize the page
+         * get title of the page
+         * get url of the page
+         * validate the title, expected title is "Google"
+         * validate the url of the page, expected url is "https://www.google.com/"
+         * close the browser
+         */
+        // in selenium 3 we had to set up the driver, for that we need webdrivermanager libraries
+        // WebDriverManager.chromedriver().setup();
+
+        // create driver object
+        WebDriver driver = new ChromeDriver();
+
+        // navigate to google
+        // driver.get()
+        driver.navigate().to("https://www.google.com");
+
+        // maximize
+        driver.manage().window().maximize();
+
+
+
+//        String expectedTitle = "Google";
+//        String actualTitle = driver.getTitle();
+//
+//        if(actualTitle.contains(expectedTitle)){
+//            System.out.println("Actual title: " + actualTitle + ", matches expected title: " + expectedTitle + ". => TEST PASS");
+//        } else {
+//            System.out.println("Actual title: " + actualTitle + ", DOES NOT match expected title: " + expectedTitle + ". => TEST FAIL");
+//        }
+//
+//        String expectedUrl = "https://www.google.com";
+//        String actualUrl = driver.getCurrentUrl();
+//
+//        if(actualUrl.contains(expectedUrl)){
+//            System.out.println("Actual url: " + actualUrl + ", matches expected url: " + expectedUrl + ". => TEST PASS");
+//        } else {
+//            System.err.println("Actual url: " + actualUrl + ", DOES NOT match expected url: " + expectedUrl + ". => TEST FAIL");
+//        }
+//
+//        driver.quit();
+    }
+}
