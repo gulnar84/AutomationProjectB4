45145a57d7f88c33144c6936338ffd22c8ba45e7
gulnar84 <gulnarresearcher@gmail.com>
2025-02-07 09:16:03 -0500

    My first automationchange2

---
+++ src/test/java/Io/loop/test/day9/T1_actions_hoverover.java
@@ -0,0 +1,14 @@
+package Io.loop.test.day9;
+
+import Io.loop.test.Utilities.Driver;
+import org.testng.annotations.Test;
+
+public class T1_actions_hoverover {
+    /*
+
+     */
+    @Test
+    public void google_actions_hover(){
+        Driver.getDriver()
+    }
+}
